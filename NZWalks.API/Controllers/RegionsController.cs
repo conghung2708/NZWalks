f9aa34b289223fd5e5b04b20f3ecb3aa24c09fdf
conghung2708 <conghung05005@gmail.com>
2024-05-12 00:00:14 +0700

    get all using DI in Regions

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _nZWalksDbContext;
+        public RegionsController(NZWalksDbContext nZWalksDbContext)
+        {
+            _nZWalksDbContext = nZWalksDbContext;
+        }
+        //GET ALL REGIONS
+        //GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _nZWalksDbContext.Regions.ToList();
+               
+            return Ok(regions);
+        }
+    }
+}
