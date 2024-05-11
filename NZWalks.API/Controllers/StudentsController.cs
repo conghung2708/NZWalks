33f2287eae175ca6fce2b8ee8c28405dbc489317
conghung2708 <conghung05005@gmail.com>
2024-05-11 22:56:45 +0700

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsName = new string[]
+                  {
+                        "John",
+                        "Alice",
+                        "Bob",
+                        "Emily",
+                      // Add more names as needed
+                  };
+            return Ok(studentsName); // Assuming you want to return the names
+        }
+    }
+}
